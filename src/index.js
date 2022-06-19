431a258c17bcde4fdf0efb54434281a3c926c081
vivajane <obuekwekosi@gmail.com>
2022-06-19 23:47:35 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
 // complete the function
 function prime (num) {
-  // code goes here
+  // function prime (num) {
+  // // program to check if a number is prime or not
+
+// take input from the user
+const number = parseInt(prompt("Enter a positive number: "));
+let isPrime = true;
+
+// check if number is equal to 1
+if (number === 1) {
+    console.log("1 is neither prime nor composite number.");
 }
+
+// check if number is greater than 1
+else if (number > 1) {
+
+    // looping through 2 to number-1
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(` number : ${number} = true`);
+    } else {
+        console.log(` number : ${number} = false`);
+    }
+}
+}
+
+function solution (arg) {
+  return prime(arg);
+}
+
+if (typeof require !== "undefined" && require.main === module) {
+  if (process.argv.length <= 2) {
+    throw Error("Require argument");
+  }
+
+  const arg = process.argv[2];
+  console.log(solution(arg));
+}
+
+module.exports = { solution };
+
 
 function solution (arg) {
   return prime(arg);
